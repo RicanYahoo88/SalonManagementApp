f1c73980afd6bf67eb11164beb5c2c8565f08354
RicanYahoo88 <132402469+RicanYahoo88@users.noreply.github.com>
2023-10-10 12:25:37 -0400

    created all services for corresponding entities

---
+++ backend/spring-boot-salonmanagementapp/src/main/java/com/codigoslatinos/springbootsalonmanagementapp/service/ZipCodeService.java
@@ -0,0 +1,37 @@
+package com.codigoslatinos.springbootsalonmanagementapp.service;
+
+import com.codigoslatinos.springbootsalonmanagementapp.dao.ZipCodeRepository;
+import com.codigoslatinos.springbootsalonmanagementapp.entity.ZipCode;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ZipCodeService {
+
+    private final ZipCodeRepository zipCodeRepo;
+
+    public ZipCodeService(ZipCodeRepository zipCodeRepo) {
+        this.zipCodeRepo = zipCodeRepo;
+    }
+
+    public ZipCode addZipCode(ZipCode zipcode){
+        zipcode.setZipcode(Integer.parseInt(UUID.randomUUID().toString()));
+        return zipCodeRepo.save(zipcode);
+    }
+    public List<ZipCode> findAllZipCodes() {
+        return zipCodeRepo.findAll();
+    }
+
+    public ZipCode updateZipCode(ZipCode zipcode){
+        return zipCodeRepo.save(zipcode);
+    }
+
+    public Optional<ZipCode> findZipCode(int zipcode){
+        return Optional.of(zipCodeRepo.getReferenceById(zipcode));
+    }
+
+    public void deleteZipCode(int zipcode){
+        zipCodeRepo.deleteById(zipcode);
+    }
+}
